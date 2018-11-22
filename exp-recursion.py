755a410f7c985ac9ce28179585c9b36c0e6c9491
Debra Beat <zeeritter@gmail.com>
2018-11-22 00:43:54 -0500

    Code has been cleaned.

--- exp-recursion.py
+++ exp-recursion.py
@@ -19,15 +19,7 @@ for j in loopList[1:]:
     sequenceList[j].append(re(nextMember))
     sequenceList[j].append(im(nextMember))
 
-    #rounded version
-    #sequenceList[j].append(round(re(nextMember),3))
-    #sequenceList[j].append(round(im(nextMember),3))
 
     print str(j) + ": " + str(sequenceList[j])
 
-    #lines 31-33 are for the rounded version to break early so if rangeLength is really long it won't fuck around doing the same thing
-
-    #if sequenceList[j][0] == (sequenceList[j-1][0] + 0.001 or sequenceList[j-1][0] - 0.001):
-        #if sequenceList[j][1] == (sequenceList[j-1][1] + 0.001 or sequenceList[j-1][1] - 0.001):
-            #break
 print "Length of mysterious number: " + str(sqrt( (sequenceList[rangeLength-1][0] * sequenceList[rangeLength-1][0]) + (sequenceList[rangeLength-1][1]* sequenceList[rangeLength-1][1]) ))
